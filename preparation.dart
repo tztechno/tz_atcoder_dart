2a7ecbe7763a5c35b9cecebe390ba96a8e656d69
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-15 15:38:01 +0900

    Update preparation.dart

--- preparation.dart
+++ preparation.dart
@@ -5,6 +5,6 @@ void main() {
   var input = stdin.readLineSync();
   var A = input!.split(' ').map((e) => int.parse(e)).toList();
   var minVal = A.reduce((value, element) => value < element ? value : element);
-  print($minVal);
+  print('$minVal');
   
 }
